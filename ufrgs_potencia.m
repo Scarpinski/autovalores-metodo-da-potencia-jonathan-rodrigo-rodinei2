815bf84faa403e29c01b8ae3660119ca0c5a4c07
Scarpinski <31480925+Scarpinski@users.noreply.github.com>
2017-11-16 18:50:28 -0200

    Update ufrgs_potencia.m

--- ufrgs_potencia.m
+++ ufrgs_potencia.m
@@ -1,6 +1,6 @@
 function [lambda,x]=ufrgs_potencia(A,x,N)
 % Acha o autovalor dominante de A e seu autovetor correspondente.
-%
+% Grupo formado por Guilherme Jhonatan Rodinei Rodrigo
 % [lambda,x]=ufrgs_potencia(A,x,N)
 
 for i=1:N
